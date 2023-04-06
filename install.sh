5e2e044100726120b5e83554b774c2c555ab9fe2
Shahryar Ahmad <shehryarshahid49@gmail.com>
2023-04-06 21:27:35 +0500

    Added commands to compile modules

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@ mkdir /opt/plutonium
 mkdir /opt/plutonium/modules
 g++ plutonium.cpp -o plutonium -ldl -DNDEBUG -O3 -w
 strip plutonium
+g++ -shared modules/math.cpp -o math.so -fPIC -DNDEBUG -O3
+g++ -shared modules/regex.cpp -o regex.so -fPIC -DNDEBUG -O3
+g++ -shared modules/json.cpp -o json.so -fPIC -DNDEBUG -O3
+g++ -shared modules/cgi.cpp -o cgi.so -fPIC -DNDEBUG -O3
+g++ -shared modules/datetime.cpp -o datetime.so -fPIC -DNDEBUG -O3
 cp plutonium /opt/plutonium/
 cp -r std /opt/plutonium/
+cp modules/*.so /opt/plutonium/modules
 sudo ln -s /opt/plutonium/plutonium /usr/bin/plutonium 
